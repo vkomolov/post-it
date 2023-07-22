0691adc56948805386c3b0d2c4fee9074378343a
Vadim Komolov <vkomolov.ua@gmail.com>
2023-07-22 04:21:40 +0300

    to sort posts by two filters

--- src/containers/HeaderFilterBar/index.js
+++ src/containers/HeaderFilterBar/index.js
@@ -2,11 +2,18 @@ import React from "react";
 import "./HeaderFilterBar.scss";
 
 const HeaderFilterBar = () => {
+  //false - sorting by title, true - sorting by user
+  const userOrTitle = true; //TODO: to Reducer
+
   return (
     <div className="header__filter-bar">
       <span className="header__filter-bar__heading">Sort By: </span>
-      <button className="button" >User</button>
-      <button className="button button_active" >Post</button>
+      <button className={ userOrTitle ? "button button_active" : "button" } >
+        User
+      </button>
+      <button className={ userOrTitle ? "button" : "button button_active" } >
+        Title
+      </button>
       <i className="material-icons icon icon_login" title="Login" >login</i>
     </div>
   );
--- src/containers/PostsWrapper/index.js
+++ src/containers/PostsWrapper/index.js
@@ -17,7 +17,7 @@ const postsArr = [
   {
     id: "2",
     userId: 2,
-    userName: "Rogger Vander",
+    userName: "Elly Vander",
     title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer biam.",
     reactions: "5",
     tags: ["history", "crime"]
@@ -41,7 +41,7 @@ const postsArr = [
   {
     id: "5",
     userId: 7,
-    userName: "Margy Polak",
+    userName: "Bill Zhorek",
     title: "Fusce sed lectus facilisis, volutpat quam at, congue risus vestibulum.",
     reactions: "4",
     tags: ["history", "American", "crime"]
@@ -49,7 +49,7 @@ const postsArr = [
   {
     id: "6",
     userId: 5,
-    userName: "John Polak",
+    userName: "Bill Polak",
     title: "Mauris massa lorem, bibendum sit amet nisl eu, sodales elementum eros.",
     reactions: "5",
     tags: ["history", "American", "love"]
@@ -65,7 +65,7 @@ const postsArr = [
   {
     id: "8",
     userId: 9,
-    userName: "Zendy Bor",
+    userName: "Freddy Bor",
     title: "Aliquam mattis sem a libero mollis iaculis quis quis orci. Aenean dui.",
     reactions: "4",
     tags: ["history"]
@@ -81,7 +81,7 @@ const postsArr = [
   {
     id: "10",
     userId: 11,
-    userName: "Rogeer Polak",
+    userName: "Antony Brain",
     title: "In nisi nulla, ultricies vel elementum vel, interdum a diam cras amet.",
     reactions: "3",
     tags: ["French", "American", "love"]
@@ -89,7 +89,7 @@ const postsArr = [
   {
     id: "11",
     userId: 5,
-    userName: "John Polak",
+    userName: "Druid Venskiy",
     title: "Quisque nisi augue, facilisis in libero et, cursus vestibulum egestas.",
     reactions: "4",
     tags: ["history"]
@@ -98,47 +98,43 @@ const postsArr = [
 const postActive = "5"
 
 const PostsWrapper = () => {
+  log("PostsWrapper inits...");
+
   //false - sorting by latest, true - sorting by raiting
-  const byRaiting = true; //TODO: to Reducer
+  const raitOrLatest = true; //TODO: to Reducer
   //false - sorting by title, true - sorting by user
-  const byUser = false; //TODO: to Reducer
+  const userOrTitle = true; //TODO: to Reducer
 
   const transitionedRef = useOpacityTransition(700);
 
-
-  /**It returns the function for sorting two objects by the certain property
-   * @param {string} prop: the property of the obj to sort by...
-   * @type {function(*): function(*, *): number}
-   */
-  const sortObjbyPropString = useCallback((prop) => {
-    log(prop, "given prop: ");
+  const sortObjByMultyProps = useCallback((raitOrLatest, userOrTitle) => {
+    const byFilter = raitOrLatest ? "reactions" : "id";
+    const byPost = userOrTitle ? "userName" : "title";
 
     return (obj1, obj2) => {
-      const text1 = obj1[prop].split(" ")[0].toLowerCase();
-      const text2 = obj2[prop].split(" ")[0].toLowerCase();
-      return (text1 > text2) ? 1 : (text1 < text2) ? -1 : 0;
+      if (+obj2[byFilter] > +obj1[byFilter]) return 1
+      else if (+obj2[byFilter] < +obj1[byFilter]) return -1
+      else {
+        const text1 = obj1[byPost].split(" ")[0].toLowerCase();
+        const text2 = obj2[byPost].split(" ")[0].toLowerCase();
+
+        return text1.localeCompare(text2);
+      }
     }
   }, []);
 
   const sortedPosts = useMemo(() => {
-    const byPost = byUser ? "username" : "title";
-    const byFilter = byRaiting ? "reactions" : "id";
-
-    const postsSorted = postsArr.sort(sortObjbyPropString(byPost));
+    const postsSorted = [...postsArr].sort(sortObjByMultyProps(raitOrLatest, userOrTitle));
 
     log(postsSorted, "postsSorted: ");
 
-    const sortedArr = postsSorted.sort((obj1, obj2) => +obj2[byFilter] - +obj1[byFilter]);
-
-    log(sortedArr, "sortedArr: ");
-
-    return sortedArr.map(data => (
+    return postsSorted.map(data => (
       <PostItem
         postData={ Object.assign(data, { postActive }) }
         key={ nanoid() }
       />
     ));
-  }, [byRaiting, byUser]);
+  }, [raitOrLatest, userOrTitle, sortObjByMultyProps]);
 
   return (
     <div className="posts-wrapper" ref={ transitionedRef }>
